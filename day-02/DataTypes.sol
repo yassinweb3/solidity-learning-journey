5f19d8c57f807bd0b69689c83eb9ecf947c8debf
yassinweb3 <yasynj248@gmail.com>
2026-08-26 20:59:21 +0300

    Day 02: Solidity data types

---
+++ day-02/DataTypes.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
+
+contract Day02 {
+uint age = 25;
+int temperature = -5;
+address userAddress ;
+bytes2 code = "hi";
+string name = "Yasen";
+}
